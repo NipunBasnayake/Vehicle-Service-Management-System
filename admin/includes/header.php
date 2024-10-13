bca6d60ad49c1dbe1b7740cf7b86da5781d637b0
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2024-10-13 20:14:25 +0530

    Amin side development started

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,153 +1,155 @@
-   <?php
+<?php
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
-if (strlen($_SESSION['odmsaid']==0)) {
-  header('location:logout.php');
-  } else{
 
+if (strlen($_SESSION['odmsaid']) == 0) {
+    header('location:logout.php');
+} else {
+?>
 
+<header id="page-header" style="background-color: #fff; border-bottom: 1px solid #ddd;">
+    <div class="content-header d-flex justify-content-between align-items-center">
+        <div class="content-header-section">
+            <ul class="adminNavBar">
+                <li>
+                    <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-expanded="false">
+                        Service <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown">
+                        <li><a href="add-services.php">Add Services</a></li>
+                        <li><a href="manage-services.php">Manage Services</a></li>
+                    </ul>
+                </li>
 
-  ?>
-            <header id="page-header">
-                <!-- Header Content -->
-                <div class="content-header">
-                    <!-- Left Section -->
-                    <div class="content-header-section">
-                        <!-- Toggle Sidebar -->
-                        <!-- Layout API, functionality initialized in Codebase() -> uiApiLayout() -->
-                        <button type="button" class="btn btn-circle btn-dual-secondary" data-toggle="layout" data-action="sidebar_toggle">
-                            <i class="fa fa-navicon"></i>
+                <li><a href="aboutus.php">About Us</a></li>
+                <li><a href="contactus.php">Contact Us</a></li>
+
+                <li>
+                    <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-expanded="false">
+                        Booking <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown">
+                        <li><a href="new-booking.php">New Bookings</a></li>
+                        <li><a href="approved-booking.php">Approved Bookings</a></li>
+                        <li><a href="cancelled-booking.php">Cancelled Bookings</a></li>
+                        <li><a href="all-booking.php">All Bookings</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-expanded="false">
+                        Messages <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown">
+                        <li><a href="unread-queries.php">Unread Messages</a></li>
+                        <li><a href="read-queries.php">Read Messages</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
+        <div class="content-header-section">
+            <div class="btn-group" role="group">
+                <?php
+                $aid = $_SESSION['odmsaid'];
+                $sql = "SELECT AdminName FROM tbladmin WHERE ID=:aid";
+                $query = $dbh->prepare($sql);
+                $query->bindParam(':aid', $aid, PDO::PARAM_STR);
+                $query->execute();
+                $results = $query->fetchAll(PDO::FETCH_OBJ);
+
+                if ($query->rowCount() > 0) {
+                    foreach ($results as $row) {
+                ?>
+                        <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" 
+                            data-toggle="dropdown" aria-expanded="false">
+                            <?php echo $row->AdminName; ?>
                         </button>
-                       
-                        <div class="btn-group" role="group">
-                            <button type="button" class="btn btn-circle btn-dual-secondary" id="page-header-options-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fa fa-wrench"></i>
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="page-header-options-dropdown">
-                                <h6 class="dropdown-header">Header</h6>
-                                <button type="button" class="btn btn-sm btn-block btn-alt-secondary" data-toggle="layout" data-action="header_fixed_toggle">Fixed Mode</button>
-                                <button type="button" class="btn btn-sm btn-block btn-alt-secondary d-none d-lg-block mb-10" data-toggle="layout" data-action="header_style_classic">Classic Style</button>
-                                <div class="d-none d-xl-block">
-                                    <h6 class="dropdown-header">Main Content</h6>
-                                    <button type="button" class="btn btn-sm btn-block btn-alt-secondary mb-10" data-toggle="layout" data-action="content_layout_toggle">Toggle Layout</button>
-                                </div>
-                                <div class="dropdown-divider"></div>
-                               
-                            </div>
-                        </div>
-                        <!-- END Layout Options -->
-
-                        <!-- Color Themes (used just for demonstration) -->
-                        <!-- Themes functionality initialized in Codebase() -> uiHandleTheme() -->
-                        <div class="btn-group" role="group">
-                            <button type="button" class="btn btn-circle btn-dual-secondary" id="page-header-themes-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fa fa-paint-brush"></i>
-                            </button>
-                            <div class="dropdown-menu min-width-150" aria-labelledby="page-header-themes-dropdown">
-                                <h6 class="dropdown-header text-center">Color Themes</h6>
-                                <div class="row no-gutters text-center mb-5">
-                                    <div class="col-4 mb-5">
-                                        <a class="text-default" data-toggle="theme" data-theme="default" href="javascript:void(0)">
-                                            <i class="fa fa-2x fa-circle"></i>
-                                        </a>
-                                    </div>
-                                    <div class="col-4 mb-5">
-                                        <a class="text-elegance" data-toggle="theme" data-theme="assets/css/themes/elegance.min.css" href="javascript:void(0)">
-                                            <i class="fa fa-2x fa-circle"></i>
-                                        </a>
-                                    </div>
-                                    <div class="col-4 mb-5">
-                                        <a class="text-pulse" data-toggle="theme" data-theme="assets/css/themes/pulse.min.css" href="javascript:void(0)">
-                                            <i class="fa fa-2x fa-circle"></i>
-                                        </a>
-                                    </div>
-                                    <div class="col-4 mb-5">
-                                        <a class="text-flat" data-toggle="theme" data-theme="assets/css/themes/flat.min.css" href="javascript:void(0)">
-                                            <i class="fa fa-2x fa-circle"></i>
-                                        </a>
-                                    </div>
-                                    <div class="col-4 mb-5">
-                                        <a class="text-corporate" data-toggle="theme" data-theme="assets/css/themes/corporate.min.css" href="javascript:void(0)">
-                                            <i class="fa fa-2x fa-circle"></i>
-                                        </a>
-                                    </div>
-                                    <div class="col-4 mb-5">
-                                        <a class="text-earth" data-toggle="theme" data-theme="assets/css/themes/earth.min.css" href="javascript:void(0)">
-                                            <i class="fa fa-2x fa-circle"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="dropdown-divider"></div>
-                                <button type="button" class="btn btn-sm btn-block btn-alt-secondary mb-10" data-toggle="layout" data-action="sidebar_style_inverse_toggle">Sidebar Style</button>
-                                <div class="dropdown-divider"></div>
-                              
-                            </div>
-                        </div>
-                        <!-- END Color Themes -->
-                    </div>
-                    <!-- END Left Section -->
-
-                    <!-- Right Section -->
-                    <div class="content-header-section">
-                        <!-- User Dropdown -->
-                        <div class="btn-group" role="group">
-                            <?php
-$aid=$_SESSION['odmsaid'];
-$sql="SELECT AdminName from  tbladmin where ID=:aid";
-$query = $dbh -> prepare($sql);
-$query->bindParam(':aid',$aid,PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $row)
-{               ?>
-                            <button type="button" class="btn btn-rounded btn-dual-secondary" id="page-header-user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <?php  echo $row->AdminName;?><i class="fa fa-angle-down ml-5"></i>
-                            </button>
-                            <?php $cnt=$cnt+1;}} ?>
-                            <div class="dropdown-menu dropdown-menu-right min-width-150" aria-labelledby="page-header-user-dropdown">
-                                <a class="dropdown-item" href="admin-profile.php">
-                                    <i class="si si-user mr-5"></i> Profile
-                                </a>
-                              
-                                <div class="dropdown-divider"></div>
-
-                                <!-- Toggle Side Overlay -->
-                                <!-- Layout API, functionality initialized in Codebase() -> uiApiLayout() -->
-                                <a class="dropdown-item" href="change-password.php" data-toggle="layout" data-action="side_overlay_toggle">
-                                    <i class="si si-wrench mr-5"></i> Settings
-                                </a>
-                                <!-- END Side Overlay -->
-
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="logout.php">
-                                    <i class="si si-logout mr-5"></i> Sign Out
-                                </a>
-                            </div>
-                        </div>
-                        <!-- END User Dropdown -->
-
-                        <!-- Toggle Side Overlay -->
-                        <!-- Layout API, functionality initialized in Codebase() -> uiApiLayout() -->
-                       
-                    </div>
-                    <!-- END Right Section -->
+                <?php
+                    }
+                }
+                ?>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a class="dropdown-item" href="admin-profile.php">
+                        <i class="fas fa-user-circle mr-2"></i> Profile
+                    </a>
+                    <a class="dropdown-item" href="change-password.php">
+                        <i class="fas fa-cog mr-2"></i> Settings
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="logout.php">
+                        <i class="fas fa-sign-out-alt mr-2"></i> Sign Out
+                    </a>
                 </div>
-                <!-- END Header Content -->
+            </div>
+        </div>
+    </div>
+</header>
 
-                
-                <!-- Header Loader -->
-                <!-- Please check out the Activity page under Elements category to see examples of showing/hiding it -->
-                <div id="page-header-loader" class="overlay-header bg-primary">
-                    <div class="content-header content-header-fullrow text-center">
-                        <div class="content-header-item">
-                            <i class="fa fa-sun-o fa-spin text-white"></i>
-                        </div>
-                    </div>
-                </div>
-                <!-- END Header Loader -->
-            </header>
-            <?php }  ?>
+<style>
+    .btn-group{
+        margin-right: 30px;;
+    }
+
+    .adminNavBar {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        background-color: #fff;
+    }
+
+    .adminNavBar > li {
+        position: relative;
+        margin-right: 20px;
+    }
+
+    .adminNavBar > li > a {
+        color: #000;
+        padding: 15px 20px;
+        text-decoration: none;
+        display: block;
+    }
+
+    .adminNavBar > li > a:hover {
+        background-color: #f0f0f0;
+    }
+
+    .dropdown {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+        display: none;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        background-color: #fff;
+        border: 1px solid #ddd;
+        min-width: 150px;
+        z-index: 1000;
+    }
+
+    .adminNavBar > li:hover .dropdown {
+        display: block;
+    }
+
+    .dropdown li {
+        padding: 0;
+    }
+
+    .dropdown a {
+        color: #000;
+        padding: 10px 15px;
+        display: block;
+    }
+
+    .dropdown a:hover {
+        background-color: #f0f0f0;
+    }
+</style>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<?php } ?>
